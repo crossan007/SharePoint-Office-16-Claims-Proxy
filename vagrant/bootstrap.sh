cf126ece4b8db93d20107a051d66d24bb0ccbb67
Charles <crossan007@gmail.com>
2017-04-25 11:13:01 -0400

    cleanup

---
+++ vagrant/bootstrap.sh
@@ -0,0 +1,34 @@
+#! /bin/bash
+
+sudo apt-get update
+sudo apt-get install -y nginx nginx-extras
+
+nginx -v
+
+#sudo openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/nginx/cert.key -out /etc/nginx/cert.crt -subj "/C=USA/ST=ANY/L=ANY/O=ANY/OU=IT Department/CN=ssl.org"
+sudo rm /etc/nginx/cert.key
+sudo rm /etc/nginx/cert.crt
+
+sudo cp ./cert.crt /etc/nginx/cert.crt
+sudo cp ./key.key /etc/nginx/cert.key
+
+sudo rm /etc/nginx/sites-enabled/*
+sudo cp ./portal.conf /etc/nginx/sites-available/portal
+sudo ln -s /etc/nginx/sites-available/portal /etc/nginx/sites-enabled/
+
+echo "####################################################"
+echo "#  nginx status:"
+sudo nginx -t
+echo "# ----------------------------------------------------#"
+echo "#  IP Addresses" 
+ifconfig  | grep 'inet addr:'| grep -v '127.0.0.1' | cut -d: -f2 | awk '{ print $1}'
+echo "#######################################################"
+
+sleep 2
+sudo rm /var/log/nginx/portal.log
+sudo touch /var/log/nginx/portal.log
+sudo chmod 777 /var/log/nginx/portal.log
+
+sudo service nginx restart
+#tail -f /var/log/nginx/portal.log
+#tail -f /var/log/nginx/error.log
