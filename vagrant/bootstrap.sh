a45ead0e0e65750e3c2af8d9aafab565e2577416
Charles Crossan <ccrossan@Bucksiu.org>
2017-04-25 13:45:44 -0400

    underp the configuration

--- vagrant/bootstrap.sh
+++ vagrant/bootstrap.sh
@@ -13,8 +13,8 @@ sudo openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/nginx/cert
 #sudo cp ./key.key /etc/nginx/cert.key
 
 sudo rm /etc/nginx/sites-enabled/*
-sudo cp ./portal.conf /etc/nginx/sites-available/portal
-sudo ln -s /etc/nginx/sites-available/portal /etc/nginx/sites-enabled/portal
+sudo cp /vagrant/vagrant/portal.conf /etc/nginx/sites-available/portal.conf
+sudo ln -s /etc/nginx/sites-available/portal.conf /etc/nginx/sites-enabled/portal
 
 echo "####################################################"
 echo "#  nginx status:"
